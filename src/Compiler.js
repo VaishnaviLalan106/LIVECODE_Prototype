526fc035215ddabf16d5083f56851f4a5b23aced
Vaishnavi Lalan <vaishnavilalan@gmail.com>
2026-05-22 15:32:22 +0530

    integrated backend with judge0 api

---
+++ src/Compiler.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import Editor from "@monaco-editor/react";
+import axios from "axios";
+
+function Compiler() {
+
+  const [language, setLanguage] = useState("python");
+
+  const [code, setCode] = useState(
+`print("Hello Vaishnavi")`
+  );
+
+  const [output, setOutput] = useState("");
+
+  const runCode = async () => {
+
+    try {
+
+      const response = await axios.post(
+        "http://localhost:5000/run",
+        {
+          code,
+          language
+        }
+      );
+
+      setOutput(response.data.output);
+
+    } catch (error) {
+
+      setOutput("Error running code");
+
+      console.log(error);
+    }
+  };
+
+  return (
+
+    <div style={{
+      background: "#1e1e1e",
+      color: "white",
+      minHeight: "100vh",
+      padding: "20px"
+    }}>
+
+      <h1>LiveCode Compiler</h1>
+
+      {/* LANGUAGE SELECT */}
+
+      <select
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
+        style={{
+          padding: "10px",
+          marginBottom: "20px"
+        }}
+      >
+
+        <option value="python">Python</option>
+        <option value="java">Java</option>
+        <option value="cpp">C++</option>
+        <option value="javascript">JavaScript</option>
+
+      </select>
+
+      {/* EDITOR */}
+
+      <Editor
+        height="400px"
+        language={language}
+        theme="vs-dark"
+        value={code}
+        onChange={(value) => setCode(value)}
+      />
+
+      {/* RUN BUTTON */}
+
+      <button
+        onClick={runCode}
+        style={{
+          marginTop: "20px",
+          padding: "10px 20px",
+          cursor: "pointer"
+        }}
+      >
+        Run Code
+      </button>
+
+      {/* TERMINAL */}
+
+      <div style={{
+        marginTop: "20px",
+        background: "black",
+        padding: "15px",
+        minHeight: "150px",
+        border: "1px solid gray"
+      }}>
+
+        <h2>Terminal</h2>
+
+        <pre>{output}</pre>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Compiler;
